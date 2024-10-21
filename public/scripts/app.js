3959fcf48193b37d1f0900948c0b20bd8f2cc8d7
Saif Al-saedi <saif4004@gmail.com>
2024-10-21 09:38:28 -0700

    Implemented the function that will add more options if the user clicked add option

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
 // Client facing scripts here
+let option_counter =3;
+//This on click function just adds another option field if the user wants more options.
+$('.add-option-btn').click(function() {
+  option_counter++;
+  const optionDiv =
+  `<div class="option">
+  <label>Option ${option_counter}</label>
+  <input type="text" name="question${option_counter}" placeholder="Title">
+  <textarea name="descrption${option_counter}" placeholder="Description (optional)"></textarea>
+  </div>
+  `;
+  $('#options').append(optionDiv);
+});
