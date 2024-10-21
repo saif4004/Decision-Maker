a6ccb6a9b0396f6df173fd3ca471432d0720f657
Saif Al-saedi <saif4004@gmail.com>
2024-10-21 10:21:56 -0700

    Added an Ajax route to send the created poll to the database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+const { types } = require("pg");
+
 // Client facing scripts here
 let option_counter =3;
 //This on click function just adds another option field if the user wants more options.
@@ -11,4 +13,27 @@ $('.add-option-btn').click(function() {
   </div>
   `;
   $('#options').append(optionDiv);
+});
+
+//Handle from submission
+$('#poll_form').submit(function(event) {
+  event.preventDefault();
+
+  const pollData = $(this).serialize(); //the $(this) refers to #poll_form
+
+  $.ajax({
+    url:'/create_poll', //poll creation route the name is just an example.
+    type: 'POST',
+    data: pollData,
+    success: function(response) {
+      // here we show the genrated link if the ajax request was a success.
+      $('#adminLink').attr('href',response.adminLink).text(response.adminLink);
+      $('#sharedLink').attr('href',response.sharedLink).text(response.sharedLink);
+      $('.result').show();
+    },
+    // This way of handling the error is mainly for debugging.
+    error: function() {
+      alert('error creating the poll');
+    }
+  });
 });
